080ce1c9054f548eb76800867c7b2961dda4c47a
zdamao <zdamao@126.com>
2020-02-16 16:33:07 +0800

    gh-pages commit

---
+++ precache-manifest.a45908c6f3099aae0fc0da67effa2192.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e97c8ca2a48c68de3fc6",
+    "url": "vue-app/css/app.39cb312a.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "vue-app/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "921ac91121d4f578e2f477afc98cca85",
+    "url": "vue-app/index.html"
+  },
+  {
+    "revision": "2adf44ff47f607f99abc",
+    "url": "vue-app/js/about.78c28de4.js"
+  },
+  {
+    "revision": "e97c8ca2a48c68de3fc6",
+    "url": "vue-app/js/app.bb9ced2c.js"
+  },
+  {
+    "revision": "00bd254ae272c75f764f",
+    "url": "vue-app/js/chunk-vendors.60dc5678.js"
+  },
+  {
+    "revision": "7ea7546e3558f668b588236c96141f7d",
+    "url": "vue-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "vue-app/robots.txt"
+  }
+]);
